2f22c7ed41fb3516812662af6260d48bb7d6db4a
suporteagp <>
2025-01-03 17:30:13 -0300

    finalizando os exercicios

--- exemplo002.py
+++ exemplo002.py
@@ -1,6 +1,9 @@
         ##Inteiros (int)
         ##Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-        a = int(input("Digite um numero: "))
+        from xmlrpc.client import boolean
+
+
+a = int(input("Digite um numero: "))
         b = int(input("Digite um numero: "))
                 
         resultado = a + b
@@ -68,47 +71,79 @@
         print(area)
 
 #Strings (str)
-#Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+        #Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
 
-a = (input("Digite seu nome:"))
+        a = (input("Digite seu nome:"))
 
-nome = a.upper()
+        nome = a.upper()
 
-print(nome)
-#Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
-a = (input("Digite seu nome:"))
+        print(nome)
+        #Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+        a = (input("Digite seu nome:"))
 
-nome = a.lower()
+        nome = a.lower()
 
-print(nome)
-#Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
-a = (input("Digite seu nome:"))
+        print(nome)
+        #Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+        a = (input("Digite seu nome:"))
 
-nome = a.strip()
+        nome = a.strip()
 
-print(nome)
-#Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
-data = input("Digite uma data no formato 'dd/mm/aaaa': ")
+        print(nome)
+        #Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+        data = input("Digite uma data no formato 'dd/mm/aaaa': ")
 
-dia, mes, ano = data.split('/')
-print(dia)
-print(mes)
-print(ano)
+        dia, mes, ano = data.split('/')
+        print(dia)
+        print(mes)
+        print(ano)
 
-#Escreva um programa que concatene duas strings fornecidas pelo usuário.
-a = str(input("Digite um nome: "))
-b = str(input("Digite um nome: "))
-                
-resultado = a + b
+        #Escreva um programa que concatene duas strings fornecidas pelo usuário.
+        a = str(input("Digite um nome: "))
+        b = str(input("Digite um nome: "))
+                        
+        resultado = a + b
 
-print(resultado)
+        print(resultado)
 #Booleanos (bool)
 #Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+verdadeiro = True
+falso = False
 
+print(verdadeiro and verdadeiro)
+print(falso and verdadeiro)
 #Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
 
+a = input("Digite o primeiro valor booleano (True/False): ").strip().capitalize()
+b = input("Digite o segundo valor booleano (True/False): ").strip().capitalize()
+
+bool1 = a == 'True'
+bool2 = b == 'True'
+
+resultado = bool1 or bool2
+
+print(resultado)
 #Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
 
-#Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+a = input("Digite o primeiro valor booleano (True/False): ").strip().capitalize()
+b = input("Digite o segundo valor booleano (True/False): ").strip().capitalize()
 
+bool1 = a == 'True'
+bool2 = b == 'True'
+
+resultado = bool1 or bool2
+
+print(not resultado)
+
+#Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
 #Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+
+a = input("Digite um numero de 1 a 5 ").strip()
+b = input("Digite um numero de 1 a 5  ").strip()
+
+if a == b:
+    print('São iguais')
+else:
+    print('São diferentes')
+
+
